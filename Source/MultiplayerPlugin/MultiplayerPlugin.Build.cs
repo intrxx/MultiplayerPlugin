f43e6a12fbc0688492fca24dfca4e04f5c186067
intrxx <michaloginski15@gmail.com>
2023-08-20 21:37:33 +0200

    Basic plugin setup

--- Source/MultiplayerPlugin/MultiplayerPlugin.Build.cs
+++ Source/MultiplayerPlugin/MultiplayerPlugin.Build.cs
@@ -8,6 +8,8 @@ public class MultiplayerPlugin : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", 
+	        "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput", "OnlineSubsystem", 
+	        "OnlineSubsystemSteam"});
     }
 }
