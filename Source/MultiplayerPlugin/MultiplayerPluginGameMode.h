6b61981126dbe9895a9ce79ee6d9772425b0f74b
intrxx <michaloginski15@gmail.com>
2023-08-20 16:14:20 +0200

    Init.

---
+++ Source/MultiplayerPlugin/MultiplayerPluginGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerPluginGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiplayerPluginGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiplayerPluginGameMode();
+};
+
+
+
